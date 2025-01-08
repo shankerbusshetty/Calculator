fa8aea9a946a6d474f7d37185ebd6b7fa9610480
Rohit <rohitprem69@gmail.com>
2025-01-08 19:10:21 +0530

    Add files via upload

---
+++ calcy.py
@@ -0,0 +1,33 @@
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.title("Simple Calculator")
+root.geometry ("570×600+100+200")
+root.resizable(False,False)
+root.configure(bg="#171616")
+
+label_result= Label(root,width=25,height=2, text="",font=("arial", 30))
+label_result. pack()
+
+Button(root, text="C", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#3697f5").place(x=10,y=100)
+Button(root, text="/", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=150,y=100)
+Button(root, text="%", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=290,y=100)
+Button(root, text="*", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=430,y=100)
+
+Button(root, text="7", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#3697f5").place(x=10,y=200)
+Button(root, text="8", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=150,y=200)
+Button(root, text="9", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=290,y=200)
+Button(root, text="-", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=430,y=200)
+
+Button(root, text="6", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#3697f5").place(x=10,y=300)
+Button(root, text="5", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=150,y=300)
+Button(root, text="4", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=290,y=300)
+Button(root, text="+", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=430,y=300)
+
+Button(root, text="3", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#3697f5").place(x=10,y=400)
+Button(root, text="2", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=150,y=400)
+Button(root, text="1", width=5, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=290,y=400)
+Button(root, text="0", width=11, height=1, font=("arial",30,"bold"), bd=1, fg="#fff",bg="#2a2d36").place(x=10,y=500)
+
+root.mainloop()
